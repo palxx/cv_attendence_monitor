4a5fb583e689938483475cceabc70721716ea396
palavirajgude <prajgude2468@gmail.com>
2023-09-27 17:47:30 +0530

    27/09/2023 547pm: started cv2 project at in india

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import os
-
 import cv2
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
 
+# importing the mode images into a list
 imgBackground = cv2.imread('resources/background.png')
 folderModePath = 'resources/Modes'
 modePathList = os.listdir(folderModePath)
@@ -13,9 +13,13 @@ imageModeList = []
 for path in modePathList:
     imageModeList.append(cv2.imread(os.path.join(folderModePath, path)))
 
+print(len(imageModeList))
+
 while True:
     success, img = cap.read()
 
-    imgBackground[162:162 + 480, 55:55+ 640] = img
-    cv2.imshow("Face attention", img)
+    imgBackground[162:162 + 480, 55:55 + 640] = img
+    imgBackground[44:44 + 633, 808:808 + 414] = imageModeList[1]
+
+    cv2.imshow("Face attention", imgBackground)
     cv2.waitKey(1)
